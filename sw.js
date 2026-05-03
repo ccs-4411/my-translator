15055be1cbcd1008f39330faa0239a993c25dbe9
ccs-4411 <ccs4411@gmail.com>
2026-05-03 16:50:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-self.addEventListener("install", e=>{
-    self.skipWaiting();
-});
-
 self.addEventListener("fetch", e=>{
+    if(e.request.url.includes("/languages") || 
+       e.request.url.includes("/tts") || 
+       e.request.method === "POST"){
+        return; // ❗ API 不攔
+    }
+
     e.respondWith(
         fetch(e.request).catch(()=>caches.match(e.request))
     );
